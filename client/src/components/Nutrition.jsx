10e8e4295200726a8557bcdba7a3d318f2018715
LenaWang03 <xianna03@gmail.com>
2023-03-12 06:40:54 -0700

    changes

--- client/src/components/Nutrition.jsx
+++ client/src/components/Nutrition.jsx
@@ -11,6 +11,11 @@ import mood from "./moodWidget.svg";
 import nutrition from "./nutritionWidget.svg";
 import symptoms from "./symptomsWidget.svg";
 import navGuy from "./navbarGuy.svg";
+import profile from "./profile.svg";
+import lemonChicken from "./lemonChicken.svg";
+import avocado from "./avocado.svg";
+import steak from "./steak.svg";
+import quinoa from "./quinoa.svg";
 
 class Dashboard extends React.Component {
   render() {
@@ -21,13 +26,18 @@ class Dashboard extends React.Component {
             <div className="header">Nutrition </div>
             <div className="date">Saturday March 11th, 2023 </div>
           </div>
-
+          <div className = "nutriText"> njfkl dhfgjshdf hsjfghlsdfg ehrjk f</div>
+          <Link to="https://www.twopeasandtheirpod.com/mediterranean-three-bean-quinoa-salad/" > <img src={lemonChicken} className="recipe" /> </Link>
+          
+          <img src={avocado} className="recipe" />
+          <img src={steak} className="recipe" />
+          <img src={quinoa} className="recipe" />
         </div>
         <div className="content">
           <div className="box1">
             <img src={dotLogoWhite} className="logo" />
             <h2>dot</h2>
-            <div id="circle">  </div>
+            <img src={profile} id="circle" />
             <p className="name"> Jane Doe </p>
 
             <p id="nav">
@@ -46,29 +56,10 @@ class Dashboard extends React.Component {
                 </span>
               </div>
             </p>
-            {/* <p className="icons">
-              <FaHome />
-              <FaCalendarAlt />
-              <FaUtensils />
-              <FaMapMarkerAlt />
-            </p> */}
             <img src={navGuy} className="navguy" />
           </div>
-          <div className="box2">
-            <img src={dashboardGuy} className="dashboardguy" />
-            <p></p>
-          </div>
-          <div className="box3">
-            <p></p>
-          </div>
-          <div className="box4">
-            <p></p>
-          </div>
-          <div className="box5">
-            <p></p>
-          </div>
+          
           <div className="box6">
-
             <p className="overview">Today's Overview</p>
             <div className = "nutrition"></div>
             <div className = "mood"></div>
