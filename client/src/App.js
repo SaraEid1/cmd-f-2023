8d0cc026bd66e0225cfe074e70175e81ba2a8eba
LenaWang03 <xianna03@gmail.com>
2023-03-12 05:17:43 -0700

    bunch of changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Calendar from "./components/Calender";
+import Nutrition from "./components/Nutrition";
 import Dashboard from "./components/Dashboard";
 import Welcome from "./components/Welcome";
 import Display from "./displayData";
@@ -14,6 +15,7 @@ class App extends React.Component {
         <Routes>
           <Route path="/calender" element={<Calendar />} /> 
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/nutrition" element={<Nutrition />} />
           <Route path="/" element={<Welcome />} />
           <Route path="/display" element={<Display />} />
         </Routes>
